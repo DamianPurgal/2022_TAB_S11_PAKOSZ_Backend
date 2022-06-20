a2dfb44123293b8f5bd2d86170e90f7fdef74610
DamianPurgal <damix242@gmail.com>
2022-06-20 20:53:36 +0200

    Reports changed to work with heroku

--- skiSlope/skiSlope/src/main/java/com/example/skiSlope/api/ReportsController.java
+++ skiSlope/skiSlope/src/main/java/com/example/skiSlope/api/ReportsController.java
@@ -43,37 +43,15 @@ public class ReportsController {
     @PreAuthorize("hasAnyRole('ROLE_MANAGER', 'ROLE_CUSTOMER')")
     public ResponseEntity<byte[]> generateCustomerReport(HttpServletResponse response) throws RuntimeException, JRException, IOException, FileNotFoundException {
 
-
         User loggedUser = (User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
         List <Card> cards= cardService.getAllCardsByUserId(loggedUser.getId());
 
         File file = ResourceUtils.getFile("src/main/resources/tickets.jrxml");
 
-
-        if(file.exists()){
-            System.out.println("exists");
-        }
-        if(file.canRead())
-        {
-            System.out.println("canRead");
-        }
-        //System.out.println(FileUtil.readAsString(file));
-
         JasperReport jasperReport = JasperCompileManager.compileReport(file.getAbsolutePath());
-
-        System.out.println("report compiled");
-
         JRBeanCollectionDataSource dataSource = new JRBeanCollectionDataSource(cards);
-        System.out.println("T1");
         Map<String, Object> parameters= new HashMap<>();
-        System.out.println("T2");
         parameters.put("Company", "Srebrne Stoki");
-        System.out.println("T3");
-        System.out.println(parameters.toString());
-        System.out.println(jasperReport.getCompileData().toString());
-        System.out.println(jasperReport.getName().toString());
-        System.out.println(jasperReport.getColumnCount());
-        System.out.println(dataSource.getData().toString());
         JasperPrint jasperPrint = null;
         try{
             jasperPrint = JasperFillManager.fillReport(jasperReport, parameters, dataSource);
@@ -82,30 +60,17 @@ public class ReportsController {
             System.out.println(e.getCause().getMessage());
         }
 
-        System.out.println("T4");
         JRPdfExporter exporter = new JRPdfExporter();
-        System.out.println("T5");
         ByteArrayOutputStream pdfOutputStream = new ByteArrayOutputStream();
-        System.out.println("T6");
         exporter.setExporterInput(new SimpleExporterInput(jasperPrint));
-        System.out.println("T7");
         exporter.setExporterOutput(new SimpleOutputStreamExporterOutput(pdfOutputStream));
-        System.out.println("T8");
         SimplePdfReportConfiguration reportConfig = new SimplePdfReportConfiguration();
-        System.out.println("T9");
         reportConfig.setSizePageToContent(true);
-        System.out.println("T10");
         reportConfig.setForceLineBreakPolicy(false);
-        System.out.println("T11");
-
         exporter.exportReport();
-        System.out.println("T12");
         byte [] res= pdfOutputStream.toByteArray();
-        System.out.println("T13");
         var headers = new HttpHeaders();
-        System.out.println("T14");
         headers.add("Content-Disposition", "inline; filename= customerReport.pdf");
-        System.out.println("T15");
         return ResponseEntity
                 .ok()
                 .headers(headers)
@@ -113,18 +78,13 @@ public class ReportsController {
                 .body(res);
     }
 
-
-
-
-
-
     @GetMapping("/manager")
     @PreAuthorize("hasAnyRole('ROLE_MANAGER')")
     public ResponseEntity<byte[]> generateManagerReport(HttpServletResponse response) throws RuntimeException, JRException, IOException, FileNotFoundException {
 
 
         List<User> users= userService.getAllUsers();
-        File file = ResourceUtils.getFile("./skiSlope/skiSlope/src/main/resources/customers.jrxml");
+        File file = ResourceUtils.getFile("src/main/resources/customers.jrxml");
 
         JasperReport jasperReport = JasperCompileManager.compileReport(file.getAbsolutePath());
 
